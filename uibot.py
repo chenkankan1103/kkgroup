864b2729ee5959182c486c8abb35218e65d36b00
chenkankan1103 <e193752468@gmail.com>
2026-02-07 02:11:54 +0800

    修復: 在 uibot.py 中添加 locker_panel 到排除列表

--- uibot.py
+++ uibot.py
@@ -59,7 +59,7 @@ async def find_and_load_extensions(base_path, package_prefix="", client=None):
     excluded_modules = {
         'cannabis_farming', 'cannabis_merchant_view', 'cannabis_merchant_view_v2',
         'cannabis_config', 'database', 'config', 'views', 'views_base',
-        'paperdoll_system', 'gambling', 'role_expiry_manager'
+        'paperdoll_system', 'gambling', 'role_expiry_manager', 'locker_panel'
     }
     
     for item in sorted(os.listdir(base_path)):
