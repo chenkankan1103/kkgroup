7c115ed95f443fe6fe9917d25978d60c7bd1f78f
chenkankan1103 <e193752468@gmail.com>
2026-03-02 19:01:48 +0800

    chore: remove identified temporary scripts

--- search_log_full.py
+++
@@ -1,8 +0,0 @@
-import sys
-path = r'C:\Users\88697\Desktop\kkgroup\bot_utf.log'
-keywords = ['重新創', '不存在', 'Forbidden', '權限']
-with open(path, encoding='utf-8', errors='ignore') as f:
-    for i, line in enumerate(f, 1):
-        if any(k in line for k in keywords):
-            print(i, line.strip())
-print('done')
