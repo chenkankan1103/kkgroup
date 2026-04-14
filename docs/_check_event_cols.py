a3e9901c8c17cb9dc2aeb38ab515d4fff1a7ecbf
Your Name <you@example.com>
2026-04-15 00:56:42 +0800

    refactor: Remove duplicate files from docs/ directory

--- docs/_check_event_cols.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('user_data.db')
-cur = conn.cursor()
-cur.execute("PRAGMA table_info(users)")
-cols = [r[1] for r in cur.fetchall()]
-event_cols = [c for c in cols if 'event' in c or 'message' in c]
-print('event/message cols:', event_cols)
-conn.close()
--- docs/inspect_db.py
+++
@@ -1,2 +0,0 @@
-# This file was used for inspecting the metrics database
-# Metrics monitoring has been disabled and removed
